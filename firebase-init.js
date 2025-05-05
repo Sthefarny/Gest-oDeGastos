bd737bcb9e24f4957602429c60a7f51780fa313a
Sthefarny <132940168+Sthefarny@users.noreply.github.com>
2025-05-05 16:57:17 -0300

    Primeiro commit do projeto

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASp-pINl0N4Jl0itRLGHJL0kXiPKhXuhA",
+    authDomain: "gastos-com-controle.firebaseapp.com",
+    projectId: "gastos-com-controle",
+    storageBucket: "gastos-com-controle.firebasestorage.app",
+    messagingSenderId: "36481635239",
+    appId: "1:36481635239:web:22bec21ca3bc62c2cbdc82"
+  };
+firebase.initializeApp(firebaseConfig);
